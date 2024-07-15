63f7bfd41991d7807a308126f337d1d2afd48d25
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-07-15 10:31:53 +0200

    Creates a basic login structure

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,34 @@
 import "../styles/form.css";
 
 export function Form() {
-  return <h1>I'm a form!</h1>;
+  function handleChange() {
+    console.log("I'm a function");
+  }
+
+  return (
+    <form id="loging-form" action="">
+      <h1>Welcome to Blisko!</h1>
+      <h2>The Ultimate Friendship Organizer</h2>
+      <h3>Log in now and discover the magic of perfectly timed hangouts!</h3>
+      <label htmlFor="username">
+        <input
+          type="text"
+          className="form__input-username"
+          name="username"
+          placeholder="Username"
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="email">
+        <input
+          type="text"
+          className="form__input-email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+      </label>
+      <button className="form__button-login">Login</button>
+    </form>
+  );
 }
