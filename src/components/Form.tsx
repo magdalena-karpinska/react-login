04d64849a82d2ace81a36e87dcdd2a8d69b90b44
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-07-15 10:51:56 +0200

    Adds basic styling and a password input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,6 +11,7 @@ export function Form() {
       <h2>The Ultimate Friendship Organizer</h2>
       <h3>Log in now and discover the magic of perfectly timed hangouts!</h3>
       <label htmlFor="username">
+        Enter your cool nickname here
         <input
           type="text"
           className="form__input-username"
@@ -20,11 +21,22 @@ export function Form() {
         />
       </label>
       <label htmlFor="email">
+        Where we send good vibes and friendship updates
         <input
           type="text"
           className="form__input-email"
           name="email"
           placeholder="Email"
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="password">
+        Shh… It’s the key to your social kingdom
+        <input
+          type="text"
+          className="form__input-password"
+          name="password"
+          placeholder="Password"
           onChange={handleChange}
         />
       </label>
