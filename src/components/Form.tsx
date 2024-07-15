f291f274451d25ec7b82d45991b75678c602ddce
Magdalena Kaprińska <159182249+magdalena-karpinska@users.noreply.github.com>
2024-07-15 11:26:09 +0200

    Creates zod validation schema

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,32 @@
 import { useState } from "react";
+import { z } from "zod";
 import "../styles/form.css";
+
+const schema = z.object({
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .max(20, "Username must not exceed 20 characters")
+    .regex(
+      /^[a-zA-Z0-9_]+$/,
+      "Username can only contain letters, numbers, and underscores"
+    ),
+
+  email: z
+    .string()
+    .email("Invalid email address")
+    .min(5, "Email must be at least 5 characters long")
+    .max(50, "Email must not exceed 50 characters"),
+
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .max(50, "Password must not exceed 50 characters")
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+      "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+    ),
+});
 
 export function Form() {
   const [formData, setFormData] = useState({
@@ -7,6 +34,7 @@ export function Form() {
     email: "",
     password: "",
   });
+
   function handleChange(
     event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) {
@@ -19,8 +47,14 @@ export function Form() {
     });
   }
 
+  async function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    const result = schema.safeParse(formData);
+  }
+
   return (
-    <form id="loging-form" action="">
+    <form id="loging-form" onSubmit={handleSubmit}>
       <h1>Welcome to Blisko!</h1>
       <h2>The Ultimate Friendship Organizer</h2>
       <h3>Log in now and discover the magic of perfectly timed hangouts!</h3>
